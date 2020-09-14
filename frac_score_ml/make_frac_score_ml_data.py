21202974a34d2beca470598aabe5b229514522dc
endlessformsOR <61475069+endlessformsOR@users.noreply.github.com>
2020-09-14 14:19:40 -0500

    Update make_frac_score_ml_data.py

--- frac_score_ml/make_frac_score_ml_data.py
+++ frac_score_ml/make_frac_score_ml_data.py
@@ -1,36 +1,39 @@
+# use this to build datasets for ML frac score.
 
-## use this to build datasets for ML frac score.
-
-import numpy as np
 import pandas as pd
-from frac_score_ml_utils import make_frac_score_data_non_events, make_frac_score_data_set
+from ml_utils import make_frac_score_data_non_events, make_frac_score_data_set
 
-# run through all events in combined efficency report, from all recorded wells
 
-df_wellIDs = pd.read_csv('wells3Sept2020.csv')
-df_effReport = pd.read_csv('fracPops_effReport_allPads_4Sept2020.csv')
+# run through all events in combined efficiency report, for all recorded wells
 
-num_windows_per_stage = 4
-counting_window_secs = 15
-STD_M = 10
-#distanceVal = 1000
-distanceVal = 4000
-timeBeforePop_secs = 2
-timeAfterPop_secs = 3
-
-make_frac_score_data_non_events(df_wellIDs, df_effReport, num_windows_per_stage, counting_window_secs, STD_M, distanceVal, timeBeforePop_secs, timeAfterPop_secs)
-
-# run through all events in combined efficency report, from all recorded wells
+df_well_ids = pd.read_csv('wells3Sept2020.csv')
+df_eff_report = pd.read_csv('fracPops_effReport_allPads_4Sept2020.csv')
 
 num_windows_per_stage = 2
 counting_window_secs = 15
-STD_M = 10
-#distanceVal = 1000
-distanceVal = 4000
-timeBeforePop_secs = 2
-timeAfterPop_secs = 3
-
+distance_val = 4000
+std_m = 10
+time_before_pop_secs = 2
+time_after_pop_secs = 3
 
 # now make data
 
-make_frac_score_data_set(df_wellIDs, df_effReport, num_windows_per_stage, counting_window_secs, STD_M, distanceVal, timeBeforePop_secs, timeAfterPop_secs)
+make_frac_score_data_set(df_well_ids,
+                         df_eff_report,
+                         num_windows_per_stage,
+                         counting_window_secs,
+                         std_m, distance_val,
+                         time_before_pop_secs,
+                         time_after_pop_secs)
+
+
+# make nonevents
+
+num_windows_per_stage = 4
+make_frac_score_data_non_events(df_well_ids,
+                                df_eff_report,
+                                num_windows_per_stage,
+                                counting_window_secs,
+                                std_m, distance_val,
+                                time_before_pop_secs,
+                                time_after_pop_secs)
